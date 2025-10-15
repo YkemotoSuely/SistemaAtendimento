e273d9fbc326f6ccfff5a3403dcae7976e328bba
Edson Rodrigues <edsonrb.2008@gmail.com>
2025-10-14 21:57:28 -0300

    Implementação do model Atendimento e Etapas do Atendimento - Listar Atendimento

---
+++ Repositories/AtendimentoRepository.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+using SistemaAtendimento.Database;
+using SistemaAtendimento.Model;
+
+namespace SistemaAtendimento.Repositories
+{
+    public class AtendimentoRepository
+    {
+        //Código do Atendimento
+        //Nome do Cliente
+        //CPF
+        //CNPJ
+        public List<Atendimentos> Listar(string termo = "", string condicao = "")
+        {
+            var lista = new List<Atendimentos>();
+            using (var conexao = ConexaoDB.GetConexao())
+            {
+                string sql = @"SELECT a.*, c.nome AS cliente_nome, s.nome AS situacao_nome, u.nome AS usuario_nome, c.cpf_cnpj 
+                            FROM atendimentos a 
+                            INNER JOIN clientes c ON c.id = a.cliente_id 
+                            INNER JOIN usuarios u ON u.id = a.usuario_id 
+                            INNER JOIN situacao_atendimentos s ON s.id = a.situacao_atendimento_id";
+
+                if (!string.IsNullOrEmpty(termo) && !string.IsNullOrEmpty(condicao))
+                {
+                    if(condicao == "Código do Atendimento")
+                    {
+                        sql += " WHERE id = @termo";
+                    }else if (condicao == "Nome do Cliente")
+                    {
+                        sql += " WHERE c.nome LIKE %@termo%";
+                    }
+                    else
+                    {
+                        sql += " WHERE c.cpf_cnpj = @termo";
+                    }
+                }
+
+                using (var comando = new SqlCommand(sql, conexao))
+                {
+                    if (!string.IsNullOrEmpty(termo))
+                    {
+                        comando.Parameters.AddWithValue("termo", termo);
+                    }
+
+                    conexao.Open();
+
+                    using (var linhas = comando.ExecuteReader())
+                    {
+
+                    }
+                }              
+
+            }
+            return lista;
+        }
+
+        public void Inserir(Atendimentos atendimento)
+        {
+
+        }
+
+        public void Atualizar(Atendimentos atendimento)
+        {
+
+        }
+
+        public void Excluir(int id)
+        {
+
+        }
+    }
+}
